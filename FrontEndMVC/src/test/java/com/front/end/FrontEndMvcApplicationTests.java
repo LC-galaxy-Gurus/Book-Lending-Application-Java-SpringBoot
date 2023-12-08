b1cca5096d297a13ea930aabdfd869b4f1e21bd2
zachjhavers <113553589+zachjhavers@users.noreply.github.com>
2023-12-08 14:39:29 -0500

    Added A Front End Template

---
+++ FrontEndMVC/src/test/java/com/front/end/FrontEndMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.front.end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
